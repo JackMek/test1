24ff35aa330797bfef4b73538b2f3d6cbb60086d
JackMek <31303858+JackMek@users.noreply.github.com>
2018-03-27 10:47:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,8 +63,8 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'list') {
        
-            message.reply(listPlayers[i-2]) 
-            message.reply(listPlayers[i-1])
+            message.reply(listPlayers[i-2]);
+            message.reply(listPlayers[i-1]);
             message.reply(listPlayers[i]);
       
   	}
