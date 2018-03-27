754d0445b9808c2858d67af85236efaae0e96d5f
JackMek <31303858+JackMek@users.noreply.github.com>
2018-03-27 12:15:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,7 +61,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'l' && message.channel.id === CHANNEL_IDc) {
+    if (message.content === 'l') {
         if (i == lenList+2){
             i = 2;
             message.reply("Possono finalizzare i seguenti utenti: " + listPlayers[i-2] + " , " + listPlayers[i-1] + " , " + listPlayers[i]);
